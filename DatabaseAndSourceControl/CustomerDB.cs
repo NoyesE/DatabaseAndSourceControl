d38f1b77e44a8d9b2cc24f4b1266e223b1d469ce
Ian Noyes <noyese18@hotmail.com>
2019-01-07 14:56:47 -0800

    Connection

---
+++ DatabaseAndSourceControl/CustomerDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAndSourceControl
+{
+    static class CustomerDB
+    {
+        public static List<string> GetAllNames()
+        {
+            SqlConnection con =
+                new SqlConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=CustomerDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+
+            using (con)
+            {
+                con.Open();
+
+            }
+        }
+    }
+}
